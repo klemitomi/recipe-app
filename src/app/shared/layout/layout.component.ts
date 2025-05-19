82c76212e0c18d0679dc7c18a23f483dada6bc6e
Klement Tamás <klemitomi84@gmail.com>
2025-05-20 00:11:46 +0200

    Initial components

---
+++ src/app/shared/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    
+  ],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {
+
+}
